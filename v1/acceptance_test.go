c750e5908068105d39b5049fb745d1a8323566f3
tyler <tyler@cruftbusters.com>
2021-12-21 19:44:54 -0600

    group  and move fixtures

--- v1/acceptance_test.go
+++ v1/acceptance_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestAcceptance(t *testing.T) {
 	t.Run("wrong aspect ratio", func(t *testing.T) {
-		err := main("wrong_aspect_ratio.tif")
+		err := main("fixtures/wrong_aspect_ratio.tif")
 		want := ErrWrongAspectRatio
 		if err != want {
 			t.Fatalf("got %s want %s", err, want)
@@ -14,7 +14,7 @@ func TestAcceptance(t *testing.T) {
 	})
 
 	t.Run("right aspect ratio", func(t *testing.T) {
-		err := main("right_aspect_ratio.tif")
+		err := main("fixtures/right_aspect_ratio.tif")
 		if err != nil {
 			t.Fatal(err)
 		}
