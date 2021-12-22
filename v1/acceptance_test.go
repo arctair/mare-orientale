63bbcbbd43469d66b1d5f9922c7e4483f699b787
tyler <tyler@cruftbusters.com>
2021-12-21 19:39:29 -0600

    print aspect ratio error only when aspect ratio is not 284.7 x 94.2

--- v1/acceptance_test.go
+++ v1/acceptance_test.go
@@ -5,9 +5,18 @@ import (
 )
 
 func TestAcceptance(t *testing.T) {
-	err := main("wrong_aspect_ratio.tif")
-	want := ErrWrongAspectRatio
-	if err != want {
-		t.Fatalf("got %s want %s", err, want)
-	}
+	t.Run("wrong aspect ratio", func(t *testing.T) {
+		err := main("wrong_aspect_ratio.tif")
+		want := ErrWrongAspectRatio
+		if err != want {
+			t.Fatalf("got %s want %s", err, want)
+		}
+	})
+
+	t.Run("right aspect ratio", func(t *testing.T) {
+		err := main("right_aspect_ratio.tif")
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
--- v1/main.go
+++ v1/main.go
@@ -2,10 +2,26 @@ package v1
 
 import (
 	"errors"
+	"os"
+
+	"golang.org/x/image/tiff"
 )
 
 func main(file string) error {
-	return ErrWrongAspectRatio
+	reader, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	image, err := tiff.Decode(reader)
+	if err != nil {
+		return err
+	}
+	bounds := image.Bounds()
+	aspectRatio := float64(bounds.Dy()) / float64(bounds.Dx())
+	if aspectRatio != 94.2/284.7 {
+		return ErrWrongAspectRatio
+	}
+	return nil
 }
 
 var ErrWrongAspectRatio = errors.New("aspect ratio must be 284.7 x 94.2")
