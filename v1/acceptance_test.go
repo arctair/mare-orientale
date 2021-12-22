903fd59db39fea0051db7c48fc88f223096f26d3
tyler <tyler@cruftbusters.com>
2021-12-21 20:58:46 -0600

    wip

--- v1/acceptance_test.go
+++ v1/acceptance_test.go
@@ -1,12 +1,13 @@
 package v1
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestAcceptance(t *testing.T) {
 	t.Run("wrong aspect ratio", func(t *testing.T) {
-		err := main("fixtures/wrong_aspect_ratio.tif")
+		_, err := main("fixtures/wrong_aspect_ratio.tif")
 		want := ErrWrongAspectRatio
 		if err != want {
 			t.Fatalf("got %s want %s", err, want)
@@ -14,9 +15,13 @@ func TestAcceptance(t *testing.T) {
 	})
 
 	t.Run("right aspect ratio", func(t *testing.T) {
-		err := main("fixtures/right_aspect_ratio.tif")
+		got, err := main("fixtures/right_aspect_ratio.tif")
 		if err != nil {
 			t.Fatal(err)
+		}
+		want := `solid wasd endsolid`
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %+v want %+v", got, want)
 		}
 	})
 }
--- v1/main.go
+++ v1/main.go
@@ -7,21 +7,29 @@ import (
 	"golang.org/x/image/tiff"
 )
 
-func main(file string) error {
+func main(file string) (string, error) {
 	reader, err := os.Open(file)
 	if err != nil {
-		return err
+		return "", err
 	}
 	image, err := tiff.Decode(reader)
 	if err != nil {
-		return err
+		return "", err
 	}
 	bounds := image.Bounds()
 	aspectRatio := float64(bounds.Dy()) / float64(bounds.Dx())
 	if aspectRatio != 94.2/284.7 {
-		return ErrWrongAspectRatio
+		return "", ErrWrongAspectRatio
 	}
-	return nil
+	return "", nil
 }
 
 var ErrWrongAspectRatio = errors.New("aspect ratio must be 284.7 x 94.2")
+
+type Quad struct {
+	V0, V1, V2, V3 Vertex
+}
+
+type Vertex struct {
+	X, Y, Z int
+}
