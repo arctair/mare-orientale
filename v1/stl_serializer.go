9e01ea73fa8c9d3f32f23feaf9a2f2c99e294292
tyler <tyler@cruftbusters.com>
2021-12-24 14:37:01 -0600

    stl serializer writes polygons as facets

---
+++ v1/stl_serializer.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func serializeToSTL(polygons [][]Vertex) string {
+	output := "solid \n"
+	for _, polygon := range polygons {
+		output = writeFacet(output, polygon)
+	}
+	output += "endsolid"
+	return output
+}
+
+func writeFacet(output string, polygon []Vertex) string {
+	output += "  facet normal 0 0 0\n    outer loop\n"
+	for _, vertex := range polygon {
+		output = writeVertex(output, vertex)
+	}
+	output += "    endloop\n  endfacet\n"
+	return output
+}
+
+func writeVertex(output string, vertex Vertex) string {
+	x, y, z := strconv.FormatFloat(vertex.X, 'f', -1, 64),
+		strconv.FormatFloat(vertex.Y, 'f', -1, 64),
+		strconv.FormatFloat(vertex.Z, 'f', -1, 64)
+	return output + fmt.Sprintf("      vertex %s %s %s\n", x, y, z)
+}
--- v1/stl_serializer_test.go
+++ v1/stl_serializer_test.go
@@ -1,15 +1,37 @@
 package v1
 
-import "testing"
-
-func serializeToSTL(polygons [][]Vertex) string {
-	return "solid \nendsolid"
-}
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSTLSerializer(t *testing.T) {
-	got := serializeToSTL(nil)
-	want := "solid \nendsolid"
-	if got != want {
-		t.Fatalf("got '%s' want '%s'", got, want)
-	}
+	t.Run("empty solid", func(t *testing.T) {
+		got := serializeToSTL(nil)
+		want := "solid \nendsolid"
+		if got != want {
+			t.Fatalf("got '%s' want '%s'", got, want)
+		}
+	})
+
+	t.Run("one facet", func(t *testing.T) {
+		got := serializeToSTL([][]Vertex{
+			{
+				{0, 1, 2},
+				{3, 4, 5},
+				{6, 7, 8},
+			},
+		})
+		wantFacet := `  facet normal 0 0 0
+    outer loop
+      vertex 0 1 2
+      vertex 3 4 5
+      vertex 6 7 8
+    endloop
+  endfacet`
+		want := fmt.Sprintf("solid \n%s\nendsolid", wantFacet)
+		if got != want {
+			t.Fatalf("got '%s' want '%s'", got, want)
+		}
+	})
 }
