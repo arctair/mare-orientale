63974baca4172474f1157506f46f9d01e4205933
tyler <tyler@cruftbusters.com>
2021-12-25 11:45:48 -0600

    refactor - create heightmap test fixtures on the fly

--- v1/sampler_test.go
+++ v1/sampler_test.go
@@ -1,20 +1,69 @@
 package v1
 
 import (
+	"bufio"
+	"image"
+	"os"
 	"testing"
+
+	"golang.org/x/image/tiff"
 )
 
 func TestSampler(t *testing.T) {
+	dir := t.TempDir()
+
 	t.Run("wrong aspect ratio", func(t *testing.T) {
-		_, err := Sampler("fixtures/wrong_aspect_ratio.tif")
+		path := dir + "/wrong_aspect_ratio.tif"
+
+		t.Run("create test heightmap", func(t *testing.T) {
+			file, err := os.Create(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer file.Close()
+
+			image := image.NewGray16(
+				image.Rectangle{
+					image.Point{0, 0},
+					image.Point{23, 34},
+				},
+			)
+
+			writer := bufio.NewWriter(file)
+			tiff.Encode(writer, image, nil)
+			writer.Flush()
+		})
+
+		_, got := Sampler(path)
 		want := ErrWrongAspectRatio
-		if err != want {
-			t.Fatalf("got %s want %s", err, want)
+		if got != want {
+			t.Fatalf("got %s want %s", got, want)
 		}
 	})
 
 	t.Run("right aspect ratio", func(t *testing.T) {
-		sampler, err := Sampler("fixtures/right_aspect_ratio.tif")
+		path := dir + "/right_aspect_ratio.tif"
+
+		t.Run("create test heightmap", func(t *testing.T) {
+			file, err := os.Create(path)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer file.Close()
+
+			image := image.NewGray16(
+				image.Rectangle{
+					image.Point{0, 0},
+					image.Point{2847, 942},
+				},
+			)
+
+			writer := bufio.NewWriter(file)
+			tiff.Encode(writer, image, nil)
+			writer.Flush()
+		})
+
+		sampler, err := Sampler(path)
 		if err != nil {
 			t.Fatal(err)
 		}
