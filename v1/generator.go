11e3bdfc7461a4f9e68a65328801432d43c1cb86
tyler <tyler@cruftbusters.com>
2021-12-25 13:08:13 -0600

    refactor - push generator math into grid declaration

--- v1/generator.go
+++ v1/generator.go
@@ -1,9 +1,9 @@
 package v1
 
 func Generate(sampler func(x, y float64) float64) [][]Vertex {
-	grid := [][]float64{
-		{sampler(0, 0), sampler(0, 18)},
-		{sampler(18, 0), sampler(18, 18)},
+	grid := [][]Vertex{
+		{Vertex{0, 0, 9.3 + sampler(0, 0)}, Vertex{0, 18, 9.3 + sampler(0, 18)}},
+		{Vertex{18, 0, 9.3 + sampler(18, 0)}, Vertex{18, 18, 9.3 + sampler(18, 18)}},
 	}
 	return [][]Vertex{
 		// bottom face
@@ -16,37 +16,37 @@ func Generate(sampler func(x, y float64) float64) [][]Vertex {
 		// left face
 		{
 			Vertex{0, 0, 0},
-			Vertex{0, 0, 9.3 + grid[0][0]},
-			Vertex{0, 18, 9.3 + grid[0][1]},
+			grid[0][0],
+			grid[0][1],
 			Vertex{0, 18, 0},
 		},
 		// back face
 		{
 			Vertex{0, 18, 0},
-			Vertex{0, 18, 9.3 + grid[0][1]},
-			Vertex{18, 18, 9.3 + grid[1][1]},
+			grid[0][1],
+			grid[1][1],
 			Vertex{18, 18, 0},
 		},
 		// right face
 		{
 			Vertex{18, 18, 0},
-			Vertex{18, 18, 9.3 + grid[1][1]},
-			Vertex{18, 0, 9.3 + grid[1][0]},
+			grid[1][1],
+			grid[1][0],
 			Vertex{18, 0, 0},
 		},
 		// front face
 		{
 			Vertex{18, 0, 0},
-			Vertex{18, 0, 9.3 + grid[1][0]},
-			Vertex{0, 0, 9.3 + grid[0][0]},
+			grid[1][0],
+			grid[0][0],
 			Vertex{0, 0, 0},
 		},
 		// top face
 		{
-			Vertex{18, 0, 9.3 + grid[1][0]},
-			Vertex{18, 18, 9.3 + grid[1][1]},
-			Vertex{0, 18, 9.3 + grid[0][1]},
-			Vertex{0, 0, 9.3 + grid[0][0]},
+			grid[1][0],
+			grid[1][1],
+			grid[0][1],
+			grid[0][0],
 		},
 	}
 }
