1ace1f684a418ab9252bf8e879ab6dc62bf90317
tyler <tyler@cruftbusters.com>
2021-12-27 07:09:47 -0600

    triangulate concave polygons

--- v1/triangulator.go
+++ v1/triangulator.go
@@ -11,17 +11,71 @@ func triangulate(polygons [][]Vertex) (triangles [][]Vertex) {
 	return
 }
 
-func triangulateOne(polygon []Vertex) [][]Vertex {
-	return [][]Vertex{
-		{
-			polygon[0],
-			polygon[1],
-			polygon[2],
-		},
-		{
-			polygon[0],
-			polygon[2],
-			polygon[3],
-		},
+func triangulateOne(polygon []Vertex) (polygons [][]Vertex) {
+	normal := polygonNormal(polygon)
+	for len(polygon) > 3 {
+		index := findEar(polygon, normal)
+		polygons = append(polygons, []Vertex{
+			polygon[index],
+			polygon[index+1],
+			polygon[index+2],
+		})
+		polygon = append(polygon[:index+1], polygon[index+2:]...)
 	}
+	return append(polygons, []Vertex{
+		polygon[0],
+		polygon[1],
+		polygon[2],
+	})
+}
+
+func polygonNormal(polygon []Vertex) (normal Vertex) {
+	for index := range polygon {
+		vertexNormal := cross(
+			polygon[index],
+			polygon[(index+1)%len(polygon)],
+			polygon[(index+2)%len(polygon)],
+		)
+		normal.X += vertexNormal.X
+		normal.Y += vertexNormal.Y
+		normal.Z += vertexNormal.Z
+	}
+	return
+}
+
+func findEar(polygon []Vertex, normal Vertex) int {
+	for index := range polygon {
+		vertexNormal := cross(
+			polygon[index],
+			polygon[(index+1)%len(polygon)],
+			polygon[(index+2)%len(polygon)],
+		)
+		if hemispaceEquals(normal, vertexNormal) {
+			return index
+		}
+	}
+	panic("bad geometry")
+}
+
+func cross(v0, v1, v2 Vertex) Vertex {
+	a, b := Vertex{v1.X - v0.X, v1.Y - v0.Y, v1.Z - v0.Z},
+		Vertex{v2.X - v1.X, v2.Y - v1.Y, v2.Z - v1.Z}
+	return Vertex{
+		a.Y*b.Z - a.Z*b.Y,
+		a.Z*b.X - a.X*b.Z,
+		a.X*b.Y - a.Y*b.X,
+	}
+}
+
+func hemispaceEquals(n0, n1 Vertex) bool {
+	return signEquals(n0.X, n1.X) && signEquals(n0.Y, n1.Y) && signEquals(n0.Z, n1.Z)
+}
+
+func signEquals(a, b float64) bool {
+	if a < 0 && b > 0 {
+		return false
+	} else if b < 0 && a > 0 {
+		return false
+	}
+	return true
 }
--- v1/triangulator_test.go
+++ v1/triangulator_test.go
@@ -7,27 +7,61 @@ import (
 
 func TestTriangulator(t *testing.T) {
 	triangulate := Triangulator()
-	got := triangulate([][]Vertex{
-		{
-			{1, 1, -1},
-			{1, 1, 1},
-			{-1, -1, 1},
-			{-1, -1, -1},
-		},
+	t.Run("convex polygon", func(t *testing.T) {
+		got := triangulate([][]Vertex{
+			{
+				{1, 1, -1},
+				{1, 1, 1},
+				{-1, -1, 1},
+				{-1, -1, -1},
+			},
+		})
+		want := [][]Vertex{
+			{
+				{1, 1, -1},
+				{1, 1, 1},
+				{-1, -1, 1},
+			},
+			{
+				{1, 1, -1},
+				{-1, -1, 1},
+				{-1, -1, -1},
+			},
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %+v want %+v", got, want)
+		}
 	})
-	want := [][]Vertex{
-		{
-			{1, 1, -1},
-			{1, 1, 1},
-			{-1, -1, 1},
-		},
-		{
-			{1, 1, -1},
-			{-1, -1, 1},
-			{-1, -1, -1},
-		},
-	}
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("got %+v want %+v", got, want)
-	}
+
+	t.Run("concave polygon", func(t *testing.T) {
+		got := triangulate([][]Vertex{
+			{
+				{-2, 0, 2},
+				{1, 0, 2},
+				{1, 0, 1},
+				{2, 0, 1},
+				{2, 0, -2},
+			},
+		})
+		want := [][]Vertex{
+			{
+				{-2, 0, 2},
+				{1, 0, 2},
+				{1, 0, 1},
+			},
+			{
+				{1, 0, 1},
+				{2, 0, 1},
+				{2, 0, -2},
+			},
+			{
+				{-2, 0, 2},
+				{1, 0, 1},
+				{2, 0, -2},
+			},
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %+v want %+v", got, want)
+		}
+	})
 }
